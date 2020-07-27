4cdc390eb865eb068a319bb0244c575f28c868c4
Richard Sprince <rsprince@MacBook-Pro.local>
2020-07-27 14:44:03 -0400

    Debugs environment file.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,28 +10,28 @@ export const urlEndpoints = {
     loginUrl:'http://localhost:4200/?code=YSdALjCJRZqokUKlTWtGWadiw3OuXuw3yaEAAAfU',
     logoutUrl: '',
     // baseUrl: 'http://localhost:4200/?code=YSdALjCJRZqokUKlTWtGWadiw3OuXuw3yaEAAAfU'
-    baseUrl: 'http://localhost:4200'
+    // baseUrl: 'http://localhost:4200'
   },
  
   // proeprties for dev
   'dlv-sys-w009-4': {
-    loginUrl:'https://fmsso-devl.fanniemae.com/as/authorization.oauth2?client_id=sso-p2x-E64-d1-oidc-FannieMaeConnect_AWS&response_type=code&scope=openid profile email phone formloginonly&redirect_uri=http://dlv-sys-w009-4:8143/fmconnect',
-    logoutUrl: 'https://fmsso-devl.fanniemae.com/idp/startSLO.ping?TargetResource=http://dlv-sys-w009-4.fanniemae.com:8143/fmconnect',
-    baseUrl: 'http://'+  window.location.hostname + ':8143'
+    // loginUrl:'https://fmsso-devl.fanniemae.com/as/authorization.oauth2?client_id=sso-p2x-E64-d1-oidc-FannieMaeConnect_AWS&response_type=code&scope=openid profile email phone formloginonly&redirect_uri=http://dlv-sys-w009-4:8143/fmconnect',
+    // logoutUrl: 'https://fmsso-devl.fanniemae.com/idp/startSLO.ping?TargetResource=http://dlv-sys-w009-4.fanniemae.com:8143/fmconnect',
+    // baseUrl: 'http://'+  window.location.hostname + ':8143'
   },
  
   // proeprties for test
   'connect-test2.fanniemae.com': {
-    loginUrl:'https://fmsso-test.fanniemae.com/as/authorization.oauth2?client_id=sso-p2x-E64-t1-oidc-FannieMaeConnect_AWS&response_type=code&scope=openid profile email phone formloginonly&redirect_uri=https://connect-test2.fanniemae.com/fmconnect',
-    logoutUrl: 'https://fmsso-test.fanniemae.com/idp/startSLO.ping?TargetResource=https://connect-test2.fanniemae.com/fmconnect',
-    baseUrl: 'https://'+  window.location.hostname
+    // loginUrl:'https://fmsso-test.fanniemae.com/as/authorization.oauth2?client_id=sso-p2x-E64-t1-oidc-FannieMaeConnect_AWS&response_type=code&scope=openid profile email phone formloginonly&redirect_uri=https://connect-test2.fanniemae.com/fmconnect',
+    // logoutUrl: 'https://fmsso-test.fanniemae.com/idp/startSLO.ping?TargetResource=https://connect-test2.fanniemae.com/fmconnect',
+    // baseUrl: 'https://'+  window.location.hostname
   },
  
 };
  
 export const environment = {
   production: false,
-  baseUrl: window.location.hostname
+  // baseUrl: window.location.hostname
 };
  
 
